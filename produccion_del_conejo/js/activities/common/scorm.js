e7cfcb2c016f39242f8aba5b8f08f34fc15063c4
Erick Ortiz <72416800+Erick0D@users.noreply.github.com>
2021-09-05 13:00:13 -0500

    Update scorm.js

--- produccion_del_conejo/js/activities/common/scorm.js
+++ produccion_del_conejo/js/activities/common/scorm.js
@@ -1,6 +1,6 @@
 
 //API Adaptador para ADL SCORM 1.2 y ADL SCORM 2004 y Flash (ActionScript 3)
-//Este script implementa diferentes aspectos de comportamiento l骻ico com鷑 de un SCO.
+//Este script implementa diferentes aspectos de comportamiento l贸gico com煤n de un SCO.
 
 //======================================================================================================================
 
@@ -8,7 +8,6 @@
 var adr_ShowError = true; //Cambiar a true para mostrar los mensajes de error
 //Mensajes de error
 //Cambiar estos valores si g_bShowApiErrors es true y necesita cambiar los mensajes de error
-var adrErrorAPINoEncontrada = "No se ha podido establecer comunicaci髇 con el servidor.\nLos datos no ser醤 guardados.";
 var g_strAPITooDeep = "Cannot find API - too deeply nested.";
 var adrErrorAPINoInicializada = "Se ha localizado el API pero no se ha podido inicializar.";
 var g_strAPISetError = "No se ha localizado el API.";
@@ -26,7 +25,7 @@ var adr_FechaInicilizacion=null;
 var adr_MaximosIntentosLocalizarAPI=50;
 var adr_VersionSCORM='2004';
 
-//variables para almacenar los nombres de las funciones del est醤dar Scorm
+//variables para almacenar los nombres de las funciones del est谩ndar Scorm
 var adr_API_Inicializar='';
 var adr_API_Finalizar='';
 var adr_API_Commit='';
@@ -38,14 +37,14 @@ var adr_API_variablesAUtilizar=null;
 
 //======================================================================================================================
 
-//Funci髇 para mostrar los errores
+//Funci贸n para mostrar los errores
 function adrMostrarError(error){
 	if (adr_ShowError){
 		alert(error);
 	}
 }
 
-//Funci髇 para iniciaizar las varibles a utilizar, estas varibles dependen de la versi髇 del Scorm a utilizar
+//Funci贸n para iniciaizar las varibles a utilizar, estas varibles dependen de la versi贸n del Scorm a utilizar
 function inicializarFunciones(){
 	if (adr_VersionSCORM=='1.2'){
 		//Funciones
@@ -68,13 +67,13 @@ function inicializarFunciones(){
 		adr_API_variablesAUtilizar['tiempo']=new Array();
 		adr_API_variablesAUtilizar['tiempo']['nombre']='cmi.core.session_time';
 		adr_API_variablesAUtilizar['tiempo']['funcion']='adrConvertirTiempo';
-		//Puntuaci髇 del alumno
+		//Puntuaci贸n del alumno
 		adr_API_variablesAUtilizar['resultado']=new Array();
 		adr_API_variablesAUtilizar['resultado']['nombre']='cmi.core.score.raw';
-		//Puntuaci髇 m醲ima
+		//Puntuaci贸n m谩xima
 		adr_API_variablesAUtilizar['maximo']=new Array();
 		adr_API_variablesAUtilizar['maximo']['nombre']='cmi.core.score.max';
-		//Puntuaci髇 m韓ima
+		//Puntuaci贸n m铆nima
 		adr_API_variablesAUtilizar['minimo']=new Array();
 		adr_API_variablesAUtilizar['minimo']['nombre']='cmi.core.score.min';
 	}
@@ -108,19 +107,19 @@ function inicializarFunciones(){
 		adr_API_variablesAUtilizar['tiempo']=new Array();
 		adr_API_variablesAUtilizar['tiempo']['nombre']='cmi.session_time';
 		adr_API_variablesAUtilizar['tiempo']['funcion']='adrConvertirTiempo';
-		//Puntuaci髇 del alumno
+		//Puntuaci贸n del alumno
 		adr_API_variablesAUtilizar['resultado']=new Array();
 		adr_API_variablesAUtilizar['resultado']['nombre']='cmi.score.raw';
-		//Puntuaci髇 m醲ima
+		//Puntuaci贸n m谩xima
 		adr_API_variablesAUtilizar['maximo']=new Array();
 		adr_API_variablesAUtilizar['maximo']['nombre']='cmi.score.max';
-		//Puntuaci髇 m韓ima
+		//Puntuaci贸n m铆nima
 		adr_API_variablesAUtilizar['minimo']=new Array();
 		adr_API_variablesAUtilizar['minimo']['nombre']='cmi.score.min';
 	}
 }
 
-//Funci髇 para comprobar si encuentra el Scorm y la versi髇 del mismo (1.2 o 2004)
+//Funci贸n para comprobar si encuentra el Scorm y la versi贸n del mismo (1.2 o 2004)
 function adrLocalizaAPI(win){
 	//Comprobar Scorm
 	while ((win.API == null) && (win.API_1484_11 == null) && (win.parent != null) && (win.parent != win)) {
@@ -131,7 +130,7 @@ function adrLocalizaAPI(win){
 		}
 		win = win.parent;
 	}
-	//Comprobar versi髇
+	//Comprobar versi贸n
 	if (win.API!= null){
 		adr_VersionSCORM='1.2';
 		return win.API;
@@ -143,12 +142,12 @@ function adrLocalizaAPI(win){
 	return null;
 }
 
-//Funci髇 para comprobar el estado de la API
+//Funci贸n para comprobar el estado de la API
 function APIOK(){
 	return ((typeof(adr_API)!= "undefined") && (adr_API != null));
 }
 
-//Funci髇 para inicilizar la sesi髇
+//Funci贸n para inicilizar la sesi贸n
 function adrInicializar(){
 	var resultado = true;
 	if (adr_Inicializado){
@@ -174,7 +173,7 @@ function adrInicializar(){
 	return true;
 }
 
-//Funci髇 para finalizar la sesi髇
+//Funci贸n para finalizar la sesi贸n
 function adrFinalizar() {
 	if ((APIOK()) && (adr_Finalizado == false)) {
 		resultado  = eval('adr_API.' + adr_API_Finalizar + '("");');
@@ -183,7 +182,7 @@ function adrFinalizar() {
 	return true;
 }
 
-//Funci髇 para pasar una variable y su valor a Scorm
+//Funci贸n para pasar una variable y su valor a Scorm
 function adrSetValue(variable,valor){
 	if (!APIOK()){
 		return false;
@@ -203,7 +202,7 @@ function adrSetValue(variable,valor){
 	return resultado;
 }
 
-//Funci髇 para obtener un valor de una variable Scorm
+//Funci贸n para obtener un valor de una variable Scorm
 function adrGetValue(variable){
 	if (!APIOK()){
 		return false;
@@ -215,7 +214,7 @@ function adrGetValue(variable){
 	return valor;
 }
 
-//Funci髇 para realizar el envio de datos al servidor
+//Funci贸n para realizar el envio de datos al servidor
 function adrCommit(){
 	if (!APIOK()){
 		return false;
@@ -224,7 +223,7 @@ function adrCommit(){
 	return resultado;
 }
 
-//Funci髇 para comprobar si la actividad ha sido superada con anterioridad
+//Funci贸n para comprobar si la actividad ha sido superada con anterioridad
 function adrComprobarEstado(){
 	if (!APIOK()){
 		return false;
@@ -237,7 +236,7 @@ function adrComprobarEstado(){
 	}
 }
 
-//Funci髇 para obtener el c骴igo de un error
+//Funci贸n para obtener el c贸digo de un error
 function adrGetLastError(codigo){
 	if (!APIOK()){
 		return false;
@@ -246,7 +245,7 @@ function adrGetLastError(codigo){
 	return resultado;
 }
 
-//Funci髇 para obtener el texto del error a partir de un c骴igo de error
+//Funci贸n para obtener el texto del error a partir de un c贸digo de error
 function adrGetErrorString(){
 	if (!APIOK()){
 		return false;
@@ -255,18 +254,18 @@ function adrGetErrorString(){
 	return resultado;
 }
 
-//Funci髇 para comprobar si se ha realizado el registro, en caso negativo realizarlo
+//Funci贸n para comprobar si se ha realizado el registro, en caso negativo realizarlo
 function comprobarRegistro()
 {
 	//LLamada al flash para comprobar si se ha registrado
 	/*Para realizar la llamada desde JavaScript a ActionScript hay que tener varias cosas en cuenta:
-		1- El nombre dado al objeto embebido en la p醙ina Web, en este caso el archivo .swf de nombre "actividad".
-		2- Modificar el par醡etro "allowScriptAccess" asign醤dole el valor "always" para que se ejecute de forma correcta
+		1- El nombre dado al objeto embebido en la p谩gina Web, en este caso el archivo .swf de nombre "actividad".
+		2- Modificar el par谩metro "allowScriptAccess" asign谩ndole el valor "always" para que se ejecute de forma correcta
 	*/
 	actividad.callJavaScript();
 }
 
-//Funci髇 para convertir el tiempo a un formato correcto, dependiendo de la versi髇 del Scorm
+//Funci贸n para convertir el tiempo a un formato correcto, dependiendo de la versi贸n del Scorm
 function adrConvertirTiempo(n){
 	if (adr_VersionSCORM=='1.2'){	
 		return MillisecondsToCMIDuration(n);
@@ -276,7 +275,7 @@ function adrConvertirTiempo(n){
 	}
 }
 
-//Funci髇 para convertir duraci髇 de milisegundos a formato 0000:00:00.00 (HHHH:MM:SS.SS)
+//Funci贸n para convertir duraci贸n de milisegundos a formato 0000:00:00.00 (HHHH:MM:SS.SS)
 function MillisecondsToCMIDuration(n) {
 	var hms = "";
 	var dtm = new Date();	dtm.setTime(n);
@@ -289,7 +288,7 @@ function MillisecondsToCMIDuration(n) {
 	return hms
 }
 
-//Funci髇 para convertir duraci髇 de milisegundos a formato PThHmMsS
+//Funci贸n para convertir duraci贸n de milisegundos a formato PThHmMsS
 function MillisecondsToCMIDuration2004(n) {
 	var hms = "";
 	var dtm = new Date();	dtm.setTime(n);
